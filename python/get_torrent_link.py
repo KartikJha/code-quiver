7ba9e4439f31f936b6a191e4d61b7aec46c1eb90
Kartik Jha <kartik.n.jha@gmail.com>
2024-01-02 01:14:11 +0530

    feat: update downloader script to handle 404

--- python/get_torrent_link.py
+++ python/get_torrent_link.py
@@ -2,13 +2,15 @@ import httpx
 import re
 import os
 
-movies = ["28_hotel_rooms_2012"]
+movies = ["predestination_2014"]
 base_url = "https://yts.autos/movies/"
 
 for movie in movies:
-    movie_renamed = "-".join(movie.split('_'))
+    movie_renamed = "-".join(movie.lower().split('_'))
     response = httpx.get(base_url+movie_renamed)
-    m = re.search('href=\"magnet(.+?)announce\"', response.text)
-    os.system("transmission-cli -w /media/kartikjha/Filesystem-2 '%s'" % "magnet"+m.group(1)+"announce")
-
+    if response.status_code == 200:
+        m = re.search('href=\"magnet(.+?)announce\"', response.text)
+        os.system("transmission-cli -w /media/kartikjha/Filesystem-2 '%s'" % "magnet"+m.group(1)+"announce")
+    else:
+        print("Movie %s not found" % movie_renamed)
     # print(m.group(1))
