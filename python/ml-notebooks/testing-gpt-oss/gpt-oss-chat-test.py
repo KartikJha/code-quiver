1c887852ab915eb1b5944aeb16d18e2bf5ad35e1
Kartik Jha <kartik.n.jha@gmail.com>
2025-09-02 22:06:35 +0530

    feat: added gpt-oss-20b unquantized testing script

---
+++ python/ml-notebooks/testing-gpt-oss/gpt-oss-chat-test.py
@@ -0,0 +1,16 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "/home/kartikjha/Documents/tools/gpt-oss-20b/original/"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+prompt = "What is the capital of France?"
+inputs = tokenizer(prompt, return_tensors="pt")
+outputs = model.generate(**inputs, max_new_tokens=50)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+
+##
+# cd /home/kartikjha/Documents/code/code-quiver ; /usr/bin/env /home/kartikjha/Documents/tools/miniconda3/bin/python /home/kartikjha/.vscode/extensions/ms-python.debugpy-2025.10.0-linux-x64/bundled/libs/debugpy/adapter/../../debugpy/launcher 57563 -- /home/kartikjha/Documents/code/code-quiver/python/ml-notebooks/testing-gpt-oss/gpt-oss-chat-test.py 
+
+##
